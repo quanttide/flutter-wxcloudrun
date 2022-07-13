12223be8d8e093772249bb0fd5022aeccee06432
Guo Zhang <zhangguo@quanttide.com>
2022-07-13 16:16:16 +0800

    design: mock and unittests for APIClient

--- test/client_test.dart
+++ test/client_test.dart
@@ -15,14 +15,36 @@ import 'client_test.mocks.dart';
 void main() {
   group('APIClient测试', () {
     WXCloudRunAPIClient apiClient = MockWXCloudRunAPIClient(apiRoot: 'api.example.com');
+
     test('正常请求', () async {
-      http.Response response = await apiClient.requestAPI('GET', 'path');
+      // 打桩
+      when(apiClient.requestAPI('GET', '/path')).thenAnswer(
+              (_) async => http.Response('OK', 200)
+      );
+      // 测试
+      http.Response response = await apiClient.requestAPI('GET', '/path');
+      expect(response.statusCode, 200);
+      expect(response.body, 'OK');
     });
     test('冷启动', () async {
-
+      // 打桩
+      when(apiClient.requestAPI('GET', '/path')).thenAnswer(
+              (_) async => http.Response('Cold Starting', 503)
+      );
+      // 测试
+      http.Response response = await apiClient.requestAPI('GET', '/path');
+      expect(response.statusCode, 503);
+      expect(response.body, 'Cold Starting');
     });
-    test('请求失败', () async{
-
+    test('请求失败', () async {
+      // 打桩
+      when(apiClient.requestAPI('GET', '/path')).thenAnswer(
+              (_) async => http.Response('Not Found', 404)
+      );
+      // 测试
+      http.Response response = await apiClient.requestAPI('GET', '/path');
+      expect(response.statusCode, 404);
+      expect(response.body, 'Not Found');
     });
   });
 }
